7d2a165cc31e04753c1db4bcc2971a23c9926bd9
Marcos-ArPo <marcos.arbin.pozo.al@iespoligonosur.org>
2025-10-02 09:32:04 +0200

    Ejercicios resueltos

--- ej1/src/main/java/prac1/fichacces.java
+++ ej1/src/main/java/prac1/fichacces.java
@@ -1,7 +1,70 @@
 package prac1;
 
+import java.io.*;
+import java.util.*;
+
 public class fichacces {
-    public static void main(String [] args) {
-        
+    public static void main(String[] args) {
+        System.out.println();
+        ej1();
+        System.out.println();
+        ej2();
+    }
+
+    public static void ej1() {
+        try (RandomAccessFile raf = new RandomAccessFile(new File("./espacios.txt"), "rw");) {
+            long pos;
+            char car;
+
+            while (raf.getFilePointer() < raf.length()) {
+                pos = raf.getFilePointer();
+                car = raf.readChar();
+
+                if (car == ' ') {
+                    raf.seek(pos);
+                    raf.writeChar(0);
+                }
+
+                if (car == ';') {
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void ej2() {
+        try (RandomAccessFile raf = new RandomAccessFile(new File("./numeros.txt"), "rw")) {
+            FileWriter fw = new FileWriter("./numeros.txt");
+            Scanner sc = new Scanner(System.in);
+
+            int n  = 1;
+
+            System.out.println("Escribe 0 para terminar");
+            while (n != 0) {
+                n = sc.nextInt();
+                fw.write(n);
+            }
+
+            long pos;
+            int car;
+
+            while (raf.getFilePointer() < raf.length()) {
+                pos = raf.getFilePointer();
+                car = raf.readInt();
+
+                if (car == 5) {
+                    raf.seek(pos);
+                    raf.writeInt(0);
+                }
+            }
+
+            fw.close();
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
