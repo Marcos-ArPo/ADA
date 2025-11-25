08e2f651bf8da29c93d68b8b700ae1b72366100b
Marcos-ArPo <marcos.arbin.pozo.al@iespoligonosur.org>
2025-11-25 17:07:05 +0100

    Ejemplo hibernate completo

--- hibernate/src/main/java/conada/operaciones/Main.java
+++ hibernate/src/main/java/conada/operaciones/Main.java
@@ -8,10 +8,36 @@ import jakarta.persistence.*;
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("miBD");
-        EntityManager em = emf.createEntityManager();
 
-        Persona p = new Persona(100, "Marcos", "calle", Date.valueOf("2005-11-28"));
+        Persona p1 = new Persona(100, "Marcos", "Calle mentirosa", Date.valueOf("2005-11-28"));
+        Persona p2 = new Persona(101, "Jose", "Calle falsa", Date.valueOf("2006-08-16"));
 
+        
+        anadir(emf.createEntityManager(), p1);
+        anadir(emf.createEntityManager(), p2);
+        borrar(emf.createEntityManager(), 100);
+    }
+
+    public static void borrar(EntityManager em, int id) {
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        Persona p = em.find(Persona.class, id);
+
+        if(p == null) {
+            System.out.println("No existe la persona");
+        } else {
+            em.remove(p);
+            System.out.println("Persona eliminada");
+        }
+
+        tx.commit();
+
+        em.close();
+    }
+
+    public static void anadir(EntityManager em, Persona p) {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
